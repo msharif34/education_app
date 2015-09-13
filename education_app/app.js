a34c55ae5c6ea6d8833ed86d1f1b335a7bfddd70
msharif34 <msharif34@yahoo.com>
2015-09-13 12:36:58 -0700

    add routes and auth

--- education_app/app.js
+++ education_app/app.js
@@ -4,12 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var morgan       = require('morgan');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var session      = require('express-session');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+// require('./routes/config/passport')(passport); // pass passport for configuration
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -17,10 +21,23 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// required for passport
+app.use(session({ 
+                  secret: 'ilovescotchscotchyscotchscotch',
+                  resave: true,
+                  saveUninitialized: true
+              })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+// routes ======================================================================
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 app.use('/', routes);
 app.use('/users', users);
