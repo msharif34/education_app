5d4705fde5c839a6f3ccfa4cd2c9da134e4dd0f9
mustafa206 <mustafa.ahmed.007@gmail.com>
2015-09-14 18:17:45 -0700

    first commit

--- education_app/models/user.js
+++ education_app/models/user.js
@@ -19,6 +19,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        models.user.hasMany(models.provider);
       }
     },
     instanceMethods: {
