f4bb9b3108d3d16aeaa9f292d5972b250725a0d0
msharif34 <msharif34@yahoo.com>
2015-09-18 11:02:24 -0700

    db changes

--- db.sql
+++ db.sql
@@ -12,18 +12,26 @@ SET client_min_messages = warning;
 SET search_path = public, pg_catalog;
 
 ALTER TABLE ONLY public.users DROP CONSTRAINT users_pkey;
+ALTER TABLE ONLY public."usersCourses" DROP CONSTRAINT "usersCourses_pkey";
 ALTER TABLE ONLY public.providers DROP CONSTRAINT providers_pkey;
+ALTER TABLE ONLY public.messages DROP CONSTRAINT messages_pkey;
 ALTER TABLE ONLY public.courses DROP CONSTRAINT courses_pkey;
 ALTER TABLE ONLY public.assets DROP CONSTRAINT assets_pkey;
 ALTER TABLE ONLY public."SequelizeMeta" DROP CONSTRAINT "SequelizeMeta_pkey";
+ALTER TABLE public."usersCourses" ALTER COLUMN id DROP DEFAULT;
 ALTER TABLE public.users ALTER COLUMN id DROP DEFAULT;
 ALTER TABLE public.providers ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.messages ALTER COLUMN id DROP DEFAULT;
 ALTER TABLE public.courses ALTER COLUMN id DROP DEFAULT;
 ALTER TABLE public.assets ALTER COLUMN id DROP DEFAULT;
 DROP SEQUENCE public.users_id_seq;
+DROP SEQUENCE public."usersCourses_id_seq";
+DROP TABLE public."usersCourses";
 DROP TABLE public.users;
 DROP SEQUENCE public.providers_id_seq;
 DROP TABLE public.providers;
+DROP SEQUENCE public.messages_id_seq;
+DROP TABLE public.messages;
 DROP SEQUENCE public.courses_id_seq;
 DROP TABLE public.courses;
 DROP SEQUENCE public.assets_id_seq;
@@ -157,6 +165,45 @@ ALTER SEQUENCE courses_id_seq OWNED BY courses.id;
 
 
 --
+-- Name: messages; Type: TABLE; Schema: public; Owner: msharif34; Tablespace: 
+--
+
+CREATE TABLE messages (
+    id integer NOT NULL,
+    first_name character varying(255),
+    last_name character varying(255),
+    email character varying(255),
+    title character varying(255),
+    message text,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL
+);
+
+
+ALTER TABLE messages OWNER TO msharif34;
+
+--
+-- Name: messages_id_seq; Type: SEQUENCE; Schema: public; Owner: msharif34
+--
+
+CREATE SEQUENCE messages_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE messages_id_seq OWNER TO msharif34;
+
+--
+-- Name: messages_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: msharif34
+--
+
+ALTER SEQUENCE messages_id_seq OWNED BY messages.id;
+
+
+--
 -- Name: providers; Type: TABLE; Schema: public; Owner: msharif34; Tablespace: 
 --
 
@@ -212,6 +259,42 @@ CREATE TABLE users (
 ALTER TABLE users OWNER TO msharif34;
 
 --
+-- Name: usersCourses; Type: TABLE; Schema: public; Owner: msharif34; Tablespace: 
+--
+
+CREATE TABLE "usersCourses" (
+    id integer NOT NULL,
+    "userId" integer,
+    "courseId" integer,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL
+);
+
+
+ALTER TABLE "usersCourses" OWNER TO msharif34;
+
+--
+-- Name: usersCourses_id_seq; Type: SEQUENCE; Schema: public; Owner: msharif34
+--
+
+CREATE SEQUENCE "usersCourses_id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE "usersCourses_id_seq" OWNER TO msharif34;
+
+--
+-- Name: usersCourses_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: msharif34
+--
+
+ALTER SEQUENCE "usersCourses_id_seq" OWNED BY "usersCourses".id;
+
+
+--
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: msharif34
 --
 
@@ -250,6 +333,13 @@ ALTER TABLE ONLY courses ALTER COLUMN id SET DEFAULT nextval('courses_id_seq'::r
 -- Name: id; Type: DEFAULT; Schema: public; Owner: msharif34
 --
 
+ALTER TABLE ONLY messages ALTER COLUMN id SET DEFAULT nextval('messages_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: msharif34
+--
+
 ALTER TABLE ONLY providers ALTER COLUMN id SET DEFAULT nextval('providers_id_seq'::regclass);
 
 
@@ -258,6 +348,13 @@ ALTER TABLE ONLY providers ALTER COLUMN id SET DEFAULT nextval('providers_id_seq
 --
 
 ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: msharif34
+--
+
+ALTER TABLE ONLY "usersCourses" ALTER COLUMN id SET DEFAULT nextval('"usersCourses_id_seq"'::regclass);
 
 
 --
@@ -304,8 +401,8 @@ SELECT pg_catalog.setval('assets_id_seq', 11, true);
 
 COPY courses (id, title, instructor, description, image, category, paid, "createdAt", "updatedAt") FROM stdin;
 1	Basic Aqeedah Course	Sheikh Ahmed Nur	Tayibun Academy is now offering a new and improved 7 level classical Fiqh beginners course. The beginners course is now taught in Somali!	images/axmed.jpg	Aqeedah	t	2015-09-15 18:11:40.22-07	2015-09-15 18:11:40.22-07
-2	Basic Fiqh Course	Sheikh Mohamed Kariye	Tayibun Academy is now offering a new and improved 7 level classical Fiqh beginners course. The beginners course is now taught in Somali!	images/kariye.jpg	Fiqh	t	2015-09-15 18:14:12.754-07	2015-09-15 18:14:12.754-07
 3	Basic Tajweed Course	Sheikh Abdiaziz Jama	Tayibun Academy is now offering a new and improved 7 level classical Fiqh beginners course. The beginners course is now taught in Somali!	images/abdiaziz.jpg	Quran	t	2015-09-15 18:16:40.965-07	2015-09-15 18:16:40.965-07
+2	Basic Fiqh Course	Sheikh Mohamed Kariye	Tayibun Academy is now offering a new and improved 7 level classical Fiqh beginners course. The beginners course is now taught in Somali!	images/kariye.jpg	Fiqh	f	2015-09-15 18:14:12.754-07	2015-09-15 18:14:12.754-07
 \.
 
 
@@ -314,6 +411,21 @@ COPY courses (id, title, instructor, description, image, category, paid, "create
 --
 
 SELECT pg_catalog.setval('courses_id_seq', 3, true);
+
+
+--
+-- Data for Name: messages; Type: TABLE DATA; Schema: public; Owner: msharif34
+--
+
+COPY messages (id, first_name, last_name, email, title, message, "createdAt", "updatedAt") FROM stdin;
+\.
+
+
+--
+-- Name: messages_id_seq; Type: SEQUENCE SET; Schema: public; Owner: msharif34
+--
+
+SELECT pg_catalog.setval('messages_id_seq', 1, false);
 
 
 --
@@ -338,14 +450,31 @@ SELECT pg_catalog.setval('providers_id_seq', 1, false);
 COPY users (id, first_name, last_name, email, password, "createdAt", "updatedAt") FROM stdin;
 1	Mukhtar	Shariff	fatuma.cadey@gmail.com	$2a$10$5UASQ62Veukq/Btej28Kfu4GSnxjQp2NjH6sXxKNmI9173Y5kbina	2015-09-15 18:18:38.85-07	2015-09-15 18:18:38.85-07
 2	Mukhtar	Shariff	msharif34@yahoo.com	$2a$10$opvld00JJEuI9v.ScEQheujRIGKkhTdHQIFNZF7OyGukE9JpiWrBy	2015-09-15 19:02:03.241-07	2015-09-15 19:02:03.241-07
+3	mukhtar	\N	\N	\N	2015-09-17 12:22:45.772-07	2015-09-17 12:22:45.772-07
 \.
+
+
+--
+-- Data for Name: usersCourses; Type: TABLE DATA; Schema: public; Owner: msharif34
+--
+
+COPY "usersCourses" (id, "userId", "courseId", "createdAt", "updatedAt") FROM stdin;
+21	2	1	2015-09-17 22:35:50.159-07	2015-09-17 22:35:50.159-07
+\.
+
+
+--
+-- Name: usersCourses_id_seq; Type: SEQUENCE SET; Schema: public; Owner: msharif34
+--
+
+SELECT pg_catalog.setval('"usersCourses_id_seq"', 21, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: msharif34
 --
 
-SELECT pg_catalog.setval('users_id_seq', 2, true);
+SELECT pg_catalog.setval('users_id_seq', 3, true);
 
 
 --
@@ -373,11 +502,27 @@ ALTER TABLE ONLY courses
 
 
 --
+-- Name: messages_pkey; Type: CONSTRAINT; Schema: public; Owner: msharif34; Tablespace: 
+--
+
+ALTER TABLE ONLY messages
+    ADD CONSTRAINT messages_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: providers_pkey; Type: CONSTRAINT; Schema: public; Owner: msharif34; Tablespace: 
 --
 
 ALTER TABLE ONLY providers
     ADD CONSTRAINT providers_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: usersCourses_pkey; Type: CONSTRAINT; Schema: public; Owner: msharif34; Tablespace: 
+--
+
+ALTER TABLE ONLY "usersCourses"
+    ADD CONSTRAINT "usersCourses_pkey" PRIMARY KEY (id);
 
 
 --
